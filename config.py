7d869120239a781f922174b49fca51e540b0db7d
Nie <marknie1998@gmail.com>
2021-02-05 15:28:16 -0600

    adding job run example

--- config.py
+++ config.py
@@ -14,14 +14,14 @@ config = Config(
                 scheduler_options="#PBS -l mem=8gb -l nodes=1:ppn=2 -N test",
                 worker_init = 
                     "\n".join(("module load gcc/6.2.0",
-                    "module load pigz"))
+                    "module load pigz")),
                     # "module load miniconda3/4.7.10",
                     # "module load R/4.0.3",
                     # "source activate /gpfs/data/gao-lab/software/local_ancest")),
-                # #nodes_per_block=1,
-                # init_blocks=1,
-                # min_blocks=1,
-                # max_blocks=23,
+                nodes_per_block=1,
+                init_blocks=1,
+                min_blocks=1,
+                max_blocks=1,
                 # parallelism=1
             )
         )
