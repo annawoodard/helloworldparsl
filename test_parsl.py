0306585e00b759a2c77a6a555dd8f3efa3d15b5a
Anna Elizabeth Woodard <awoodard@nd.edu>
2021-02-07 13:19:25 -0600

    Add clarifying debug statements to test script
    Also: add a wait statement to ensure all tasks finish before script
    exits.

--- test_parsl.py
+++ test_parsl.py
@@ -8,12 +8,19 @@ from parsl import python_app, bash_app, File
 from config import config
 
 
+@python_app
+def cwd():
+    import os
+    return os.getcwd()
+
 @bash_app
-def compress(inputs = [], decompress = ""):
+def compress(inputs = [], decompress = "", stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME):
+    import os
+    print("cwd is: {}".format(os.getcwd()))
     return f"pigz {decompress} {inputs[0]}"
 
 @bash_app
-def write_hello(inputs = [], outputs = [], stdout = "~/test.out"):
+def write_hello(inputs = [], outputs = [], stderr=parsl.AUTO_LOGNAME, stdout=parsl.AUTO_LOGNAME):
     print(outputs)
     return f"cat {inputs[0]} > {outputs[0]}"
 
@@ -23,10 +30,13 @@ def write_hello(inputs = [], outputs = [], stdout = "~/test.out"):
 if __name__ == "__main__":
     parsl.set_stream_logger()
     parsl.load(config)
-    print(os.getcwd())
+    print('main process running in: {}'.format(os.getcwd()))
+    print('worker processes running in: {}'.format(cwd().result()))
     out = [File(os.path.join(os.getcwd(), "test.txt"))]
     in_ = [File(os.path.join(os.getcwd(), "hw.txt"))]
     wh = write_hello(inputs = in_, outputs = out)
     with open(wh.outputs[0].result(), 'r') as f:
         print(f.read())
     compress(inputs = wh.outputs)
+
+    parsl.wait_for_current_tasks()
